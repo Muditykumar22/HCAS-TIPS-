73716a4a5041df2b1f74e6cf64b7344441336f29
Muditykumar22 <mudity.kumar_cs22@gla.ac.in>
2026-06-22 06:16:07 +0100

    updates 10

---
+++ lec 10/hello.js
@@ -0,0 +1,88 @@
+a=10;
+console.log(a);
+console.log("hello world")
+// ussage of java script -
+// Form validation
+// Interactive websites
+// Games and animations
+// Web applications
+// Backend development using Node.js
+// Mobile app development
+
+
+// Variable is JavaScript
+// var- Can be redeclared.
+//      Function scoped.
+var name = "mudity";
+{
+name = 20;
+console.log(name);
+}
+// let - Cannot be redeclared.
+//       Block scoped.
+
+let age = 22;
+age = 23
+console.log(age);
+
+// const- Value cannot be reassigned.
+//        Block scoped.
+const PI = 3.14;
+PI= 5;
+console.log(PI);
+
+// primitive Data types in JS
+
+//  num- 
+let marks = 95;
+
+// string
+let naam= "Aman";
+
+// boolean-
+let isPassed = true;
+
+// undefined-
+// let x;
+// console.log(x);
+
+// Null-
+
+let y = null;
+
+// bigINT-
+let big = 12345678901234567890n;
+
+// Symbol-
+let id = Symbol("id");
+
+
+// Non- primitive data types-
+// Object
+let student = {
+    name: "Aman",
+    age: 22
+};
+
+console.log(student.name);
+
+// Operators in JavaScript
+
+let a = 10;
+let b = 5;
+
+console.log(a + b); //15
+console.log(a - b); //5
+console.log(a * b); //50
+console.log(a / b); //2
+console.log(a % b); //0
+
+// Assignment Operators
+
+var x = 10;
+
+x += 5; //15
+// x -= 3; //12
+// x *= 2; //24
+// x /= 4; //6
+console.log(x);
